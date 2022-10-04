533a6d793480d4b2467f9ac933b455a325ffb0f6
Stuart Murray <stuart.murray.dev@outlook.com>
2022-10-04 12:10:25 +0100

    Kernel Form

---
+++ Milestone5/ImageProcessor/KernelForm.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace image_processor
+{
+    partial class KernelForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.okButton = new System.Windows.Forms.Button();
+            this.valueTextBox = new System.Windows.Forms.TextBox();
+            this.captionLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.weightTextBox = new System.Windows.Forms.TextBox();
+            this.offsetTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.presetsComboBox = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.cancelButton.Location = new System.Drawing.Point(197, 255);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 23);
+            this.cancelButton.TabIndex = 7;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            // 
+            // okButton
+            // 
+            this.okButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.okButton.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.okButton.Location = new System.Drawing.Point(116, 255);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(75, 23);
+            this.okButton.TabIndex = 6;
+            this.okButton.Text = "OK";
+            this.okButton.UseVisualStyleBackColor = true;
+            // 
+            // valueTextBox
+            // 
+            this.valueTextBox.AcceptsReturn = true;
+            this.valueTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.valueTextBox.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.valueTextBox.Location = new System.Drawing.Point(12, 64);
+            this.valueTextBox.Multiline = true;
+            this.valueTextBox.Name = "valueTextBox";
+            this.valueTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.valueTextBox.Size = new System.Drawing.Size(260, 120);
+            this.valueTextBox.TabIndex = 5;
+            this.valueTextBox.Text = "A\r\nB\r\nC\r\nD\r\nE\r\nF";
+            // 
+            // captionLabel
+            // 
+            this.captionLabel.AutoSize = true;
+            this.captionLabel.Location = new System.Drawing.Point(12, 48);
+            this.captionLabel.Name = "captionLabel";
+            this.captionLabel.Size = new System.Drawing.Size(40, 13);
+            this.captionLabel.TabIndex = 4;
+            this.captionLabel.Text = "Kernel:";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 193);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Weight:";
+            // 
+            // weightTextBox
+            // 
+            this.weightTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.weightTextBox.Location = new System.Drawing.Point(62, 190);
+            this.weightTextBox.Name = "weightTextBox";
+            this.weightTextBox.Size = new System.Drawing.Size(31, 20);
+            this.weightTextBox.TabIndex = 9;
+            this.weightTextBox.Text = "1";
+            this.weightTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // offsetTextBox
+            // 
+            this.offsetTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.offsetTextBox.Location = new System.Drawing.Point(62, 216);
+            this.offsetTextBox.Name = "offsetTextBox";
+            this.offsetTextBox.Size = new System.Drawing.Size(31, 20);
+            this.offsetTextBox.TabIndex = 11;
+            this.offsetTextBox.Text = "0";
+            this.offsetTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 219);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Offset:";
+            // 
+            // presetsComboBox
+            // 
+            this.presetsComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.presetsComboBox.FormattingEnabled = true;
+            this.presetsComboBox.Location = new System.Drawing.Point(63, 12);
+            this.presetsComboBox.Name = "presetsComboBox";
+            this.presetsComboBox.Size = new System.Drawing.Size(121, 21);
+            this.presetsComboBox.TabIndex = 12;
+            this.presetsComboBox.SelectedIndexChanged += new System.EventHandler(this.presetsComboBox_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Presets:";
+            // 
+            // KernelForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.cancelButton;
+            this.ClientSize = new System.Drawing.Size(284, 292);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.presetsComboBox);
+            this.Controls.Add(this.offsetTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.weightTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.okButton);
+            this.Controls.Add(this.valueTextBox);
+            this.Controls.Add(this.captionLabel);
+            this.Name = "KernelForm";
+            this.Text = "Kernel";
+            this.Load += new System.EventHandler(this.KernelForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.Button okButton;
+        internal System.Windows.Forms.TextBox valueTextBox;
+        internal System.Windows.Forms.Label captionLabel;
+        internal System.Windows.Forms.Label label1;
+        internal System.Windows.Forms.Label label2;
+        internal System.Windows.Forms.TextBox weightTextBox;
+        internal System.Windows.Forms.TextBox offsetTextBox;
+        private System.Windows.Forms.ComboBox presetsComboBox;
+        internal System.Windows.Forms.Label label3;
+    }
+}
---
+++ Milestone5/ImageProcessor/KernelForm.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace image_processor
+{
+    public partial class KernelForm : Form
+    {
+        public KernelForm()
+        {
+            InitializeComponent();
+        }
+
+        // The preset kernels.
+        private string[,] Presets =
+        {
+            {"Emboss 1", "-1, 0, 0\r\n 0, 0, 0\r\n 0, 0, 1", "1", "127"},
+            {"Emboss 2", " 2, 0, 0\r\n 0,-1, 0\r\n 0, 0,-1", "1", "127"},
+            {"Gaussian Blur 5x5", "1,  4,  7,  4, 1\r\n4, 16, 26, 16, 4\r\n7, 26, 41, 26, 7\r\n4, 16, 26, 16, 4\r\n1,  4,  7,  4, 1", "273", "0"},
+            {"Box Blur 5x5", "1, 1, 1, 1, 1\r\n1, 1, 1, 1, 1\r\n1, 1, 1, 1, 1\r\n1, 1, 1, 1, 1\r\n1, 1, 1, 1, 1", "25", "0"},
+            {"High Pass 1", "-1,  2, -1\r\n-2, 12, -2\r\n-1,  2, -1", "16", "63"},
+            {"High Pass 2", " 0, -1, 0\r\n-1,  4, -1\r\n 0, -1, 0", "1", "128"},
+            {"Edge Detector NW", "-5, 0, 0\r\n 0, 0, 0\r\n 0, 0, 5", "1", "0"},
+            {"Edge Detector N",  "-1,-1,-1\r\n 0, 0, 0\r\n 1, 1, 1", "1", "0"},
+            {"Edge Detector NE", " 0, 0,-5\r\n 0, 0, 0\r\n 5, 0, 0", "1", "0"},
+            {"Edge Detector E",  " 1, 0,-1\r\n 1, 0,-1\r\n 1, 0,-1", "1", "0"},
+            {"Edge Detector SE", " 5, 0, 0\r\n 0, 0, 0\r\n 0, 0,-5", "1", "0"},
+            {"Edge Detector S",  " 1, 1, 1\r\n 0, 0, 0\r\n-1,-1,-1", "-1", "0"},
+            {"Edge Detector SW", " 0, 0, 5\r\n 0, 0, 0\r\n-5, 0, 0", "1", "0"},
+            {"Edge Detector W",  "-1, 0, 1\r\n-1, 0, 1\r\n-1, 0, 1", "1", "0"},
+        };
+
+        // Initialize the presets and select the first one.
+        private void KernelForm_Load(object sender, EventArgs e)
+        {
+            int numPresets = Presets.GetUpperBound(0) + 1;
+            for (int i = 0; i < numPresets; i++)
+            {
+                presetsComboBox.Items.Add(Presets[i, 0]);
+            }
+            presetsComboBox.SelectedIndex = 0;
+        }
+
+        //  Display the selected preset's values.
+        private void presetsComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int i = presetsComboBox.SelectedIndex;
+            valueTextBox.Text = Presets[i, 1];  // Kernel
+            weightTextBox.Text = Presets[i, 2]; // Weight
+            offsetTextBox.Text = Presets[i, 3]; // Offset
+        }
+    }
+}
